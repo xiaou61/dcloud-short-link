93a6523538025f8e028e3f628cea66715b431446
aliyun1153623907 <3153566913@qq.com>
2024-03-21 13:15:06 +0800

    框架

---
+++ dcloud-gateway/src/main/java/com/xiaou/App.java
@@ -0,0 +1,13 @@
+package com.xiaou;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
